408653f74a32b24e3a2c051eb0963e860df226b0
M. Hasnain Memon <workwithhasnain@gmail.com>
2025-11-29 09:58:20 +0500

    sorting fronetend issues season 1

--- index.php
+++ index.php
@@ -428,7 +428,7 @@ if ($_POST && isset($_POST['action'])) {
                     </div>
                 </div>
             <?php endforeach; ?>
-            <div class="col-md-4 col-5 d-none d-lg-block" style="position: relative; left: 40px;">
+            <div class="col-md-4 col-5 d-none d-lg-block" style="position: relative; left: 20px;">
                 <button class="solid w-100 py-2">
                     <i class="fa-solid fa-mobile fa-sm mx-2" style="color: white;"></i>
                     Phone Finder</button>
