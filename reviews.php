a42ac21dd26339ee782b78710c1f09c07238d49c
M. Hasnain Memon <workwithhasnain@gmail.com>
2025-12-02 14:36:51 +0500

    Adjust layout positioning and margin for improved UI consistency

--- reviews.php
+++ reviews.php
@@ -388,7 +388,7 @@ $brands = $brands_stmt->fetchAll();
                 </div>
 
             </div>
-            <div class="col-md-4 col-5 d-none d-lg-block" style="position: relative; left: 25px;">
+            <div class="col-md-4 col-5 d-none d-lg-block" style="position: relative; left: 10px;">
                 <button class="solid w-100 py-2">
                     <i class="fa-solid fa-mobile fa-sm mx-2" style="color: white;"></i>
                     Phone Finder</button>
@@ -421,7 +421,7 @@ $brands = $brands_stmt->fetchAll();
         </div>
     </div>
     <div class="container mt-0  ">
-        <div class="row">
+        <div class="row" style="margin-top: 20px;">
             <?php
             if (empty($posts)):
             ?>
