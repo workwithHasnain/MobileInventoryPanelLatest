50463387af65b02e51273daa6ac13347b33714e3
M. Hasnain Memon <79682339+workwithHasnain@users.noreply.github.com>
2026-04-07 07:14:08 +0500

    feat: implement new responsive homepage redesign with updated navigation and layout structure

--- redesign/index.php
+++ redesign/index.php
@@ -445,11 +445,26 @@ $brands = $brands_stmt->fetchAll();
           <?php if (empty($latestDevices)): ?>
             <div class="da-empty"><i class="fa fa-mobile-alt"></i>No devices yet.</div>
           <?php else: ?>
-          <div class="da-device-grid">
-            <?php foreach ($latestDevices as $device): ?>
-            <a href="<?php echo $base; ?>device/<?php echo urlencode($device['slug']); ?>" class="da-device-thumb">
-              <img src="<?php echo htmlspecialchars(getAbsoluteImagePath($device['image'], $base)); ?>" alt="<?php echo htmlspecialchars($device['name']); ?>" loading="lazy"/>
-              <span><?php echo htmlspecialchars($device['name']); ?></span>
+          <div class="da-device-list">
+            <?php foreach (array_slice($latestDevices, 0, 8) as $device): ?>
+            <a href="<?php echo $base; ?>device/<?php echo urlencode($device['slug']); ?>" class="da-device-row">
+              <div class="da-device-img-wrapper">
+                <img src="<?php echo htmlspecialchars(getAbsoluteImagePath($device['image'] ?? '', $base)); ?>" alt="<?php echo htmlspecialchars($device['name']); ?>" loading="lazy"/>
+              </div>
+              <div class="da-device-info">
+                <div class="da-device-name"><?php echo htmlspecialchars($device['name']); ?></div>
+                <div class="da-device-specs">
+                  <?php if (!empty($device['display_size'])): ?>
+                  <div class="da-device-spec-item"><i class="fa fa-mobile-screen"></i> <?php echo $device['display_size']; ?>"</div>
+                  <?php endif; ?>
+                  <?php if (!empty($device['ram'])): ?>
+                  <div class="da-device-spec-item"><i class="fa fa-memory"></i> <?php echo $device['ram']; ?></div>
+                  <?php endif; ?>
+                  <?php if (!empty($device['battery_capacity'])): ?>
+                  <div class="da-device-spec-item"><i class="fa fa-battery-full"></i> <?php echo $device['battery_capacity']; ?></div>
+                  <?php endif; ?>
+                </div>
+              </div>
             </a>
             <?php endforeach; ?>
           </div>
