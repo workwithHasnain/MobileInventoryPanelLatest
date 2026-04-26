e64f6251c354085cac1b72f984983c4b422143bc
M. Hasnain Memon <79682339+workwithHasnain@users.noreply.github.com>
2026-04-26 20:11:18 +0500

    ..

--- redesign/compare.php
+++ redesign/compare.php
@@ -388,7 +388,6 @@ $da_active_nav = 'compare';
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
 <link rel="stylesheet" href="<?php echo $base; ?>redesign/style.css">
-<link rel="stylesheet" href="<?php echo $base; ?>redesign/compare.css">
 
 <!-- Theme init (prevents FOUC) -->
 <script>
